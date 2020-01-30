8a3e2fc96e22b8296ab509b03a2687f280885e0e
JacksonMinecraft <54997459+JacksonMinecraft@users.noreply.github.com>
2020-01-30 13:01:10 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,46 +1,39 @@
-var blobs = [], arr = 0;
+var blobs = [], arr = 0, h = [];
 
 function setup() {
-  createCanvas(300, 300);
+  createCanvas(window.innerWidth, window.innerHeight);
   colorMode(HSB);
-  for (var i = 0; i < 3; i++) {
-    blobs.push(new Blob(random(0, 300), random(0, 300)));
+  for (var i = 0; i < 6; i++) {
+    blobs.push(new Blob(random(0, window.innerWidth), random(0, window.innerHeight)));
+    h.push(random(0, 360));
   }
 }
 
 function keyPressed() {
   if (keyCode === ENTER) {
-    if (blobs.length === 6) {
-      blobs.splice(arr, 1, new Blob(random(0, 300), random(0, 300)));
+    if (blobs.length === 10) {
+      blobs.splice(arr, 1, new Blob(random(0, window.innerWidth), random(0, window.innerHeight)));
+      h.splice(arr, 1, random(0, 360));
       arr++;
-      if (arr === 6) {
+      if (arr === 10) {
         arr = 0;
       }
     } else {
-      blobs.push(new Blob(random(0, 300), random(0, 300)));
+      blobs.push(new Blob(random(0, window.innerWidth), random(0, window.innerHeight)));
+      h.push(random(0, 360));
     }
   }
   console.log(blobs.length);
 }
 
 function draw() {
-  background(220);
+  background(0);
 
-  loadPixels();
-  for (x = 0; x < 300; x++) {
-    for (y = 0; y < 300; y++) {
-      let sum = 0;
-      for (i = 0; i < blobs.length; i++) {
-        let xDif = x - blobs[i].x;
-        let yDif = y - blobs[i].y;
-        let d = sqrt(pow(xDif, 2) + pow(yDif, 2));
-        sum += 10 * blobs[i].r / d;
-      }
-      set(x, y, color(sum, 255, 255));
-    }
-  }
-  updatePixels();
   for (i = 0; i < blobs.length; i++) {
+    for (j = blobs[i].r; j > 0; j--) {
+      stroke(j - 50 + h[i], 255, 255);
+      ellipse(blobs[i].x, blobs[i].y, j * 2, j * 2);
+    }
     blobs[i].update();
   }
 }
